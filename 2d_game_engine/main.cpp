847dc52448f2741c7932aea4ccfef77e40c42b2e
Matthew McCord <mattm.eternal@gmail.com>
2020-07-21 22:23:20 -0700

    Initial Commit

---
+++ 2d_game_engine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  2d_game_engine
+//
+//  Created by Matthew mccord on 7/21/20.
+//  Copyright © 2020 MCCORDINATOR. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
