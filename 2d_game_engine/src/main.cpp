e6fe5616b8c57e5c7b8c575007477c77da19344d
Matthew McCord <mattm.eternal@gmail.com>
2020-08-09 19:31:07 -0700

    simple 60 fps game loop

--- 2d_game_engine/src/main.cpp
+++ 2d_game_engine/src/main.cpp
@@ -1,18 +1,34 @@
 #include "Game.hpp"
 
+Game *game = nullptr;
+
 int main(int argc, const char * argv[]) {
-    Game game;
+    const int FPS = 60;
+    const int frameDelay = 1000 / FPS;
+    Uint32 frameStart;
+    int frameTime;
     
-    game.init("McEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+    game = new Game();
+    game->init("McEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
     
-    while (game.running())
+    while (game->running())
     {
-        game.handleEvents();
-        game.update();
-        game.render();
+        frameStart = SDL_GetTicks();
+        
+        game->handleEvents();
+        game->update();
+        game->render();
+        
+        frameTime = SDL_GetTicks() - frameStart;
+        
+        if (frameDelay > frameTime)
+        {
+            SDL_Delay(frameDelay - frameTime);
+        }
     }
     
-    game.clean();
+    game->clean();
+    delete game;
     
     return 0;
 }
